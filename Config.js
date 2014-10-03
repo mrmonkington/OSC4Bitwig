32673f3a7b00237b3de62cc2561613119f67f0d1
Jürgen Moßgraber <juergen.mossgraber@googlemail.com>
2014-10-03 21:53:12 +0200

    Added configuration of hosts and ports.

--- Config.js
+++ Config.js
@@ -27,28 +27,28 @@ Config.init = function ()
 {
     var prefs = host.getPreferences ();
 
-    Config.receiveHostSetting = prefs.getStringSetting ('Host', 'Receive from', 15, '127.0.0.1');
+    Config.receiveHostSetting = prefs.getStringSetting ('Host', 'Receive from (Script restart required)', 15, '127.0.0.1');
     Config.receiveHostSetting.addValueObserver (function (value)
     {
         Config.receiveHost = value;
         Config.notifyListeners (Config.RECEIVE_HOST);
     });
     
-    Config.receivePortSetting = prefs.getNumberSetting ('Port', 'Receive from', 0, 65535, 1, '', 8000);
+    Config.receivePortSetting = prefs.getNumberSetting ('Port', 'Receive from (Script restart required)', 0, 65535, 1, '', 8000);
     Config.receivePortSetting.addRawValueObserver (function (value)
     {
         Config.receivePort = Math.floor (value);
         Config.notifyListeners (Config.RECEIVE_PORT);
     });
     
-    Config.sendHostSetting = prefs.getStringSetting ('Host', 'Send to', 15, '127.0.0.1');
+    Config.sendHostSetting = prefs.getStringSetting ('Host', 'Send to (Script restart required)', 15, '127.0.0.1');
     Config.sendHostSetting.addValueObserver (function (value)
     {
         Config.sendHost = value;
         Config.notifyListeners (Config.SEND_HOST);
     });
     
-    Config.sendPortSetting = prefs.getNumberSetting ('Port', 'Send to', 0, 65535, 1, '', 9000);
+    Config.sendPortSetting = prefs.getNumberSetting ('Port', 'Send to (Script restart required)', 0, 65535, 1, '', 9000);
     Config.sendPortSetting.addRawValueObserver (function (value)
     {
         Config.sendPort = Math.floor (value);
